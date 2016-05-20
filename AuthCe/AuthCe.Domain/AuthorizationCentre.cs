d6a11c6f757f3112bd63b8dac5cd0b2b78c94ff2
predssj4 <adrian111217@gmail.com>
2016-05-20 20:19:00 +0200

    Rozpoczecie projektu

---
+++ AuthCe/AuthCe.Domain/AuthorizationCentre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthCe.Domain
+{
+    public class AuthorizationCentre
+    {
+        private List<Company> CompaniesUsingThisAuthCentre = new List<Company>();
+
+    }
+}
